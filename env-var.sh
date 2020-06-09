cf27a138d3136f941eebf07ec697952edd241e7d
lspacagn <lee@spacagna.co.uk>
2020-06-09 11:30:26 +0100

    Added script for setting up environmnet variables

---
+++ env-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#
+# Authentication and Access
+export APPDYNAMICS_GLOBAL_ACCOUNT_NAME=xxxxxx_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+export APPDYNAMICS_ANALYTICS_URL=https://analytics.api.appdynamics.com
+export APPDYNAMICS_ANALYTICS_API_KEY=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+# Run by calling $source env-var.sh
